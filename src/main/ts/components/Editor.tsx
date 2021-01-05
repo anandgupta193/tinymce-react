9166a35bf99acfaf448fd375923d8e7abd268b70
Anand Gupta <aanand.gupta03@gmail.com>
2021-01-05 18:03:43 +0530

    Removed URL

--- src/main/ts/components/Editor.tsx
+++ src/main/ts/components/Editor.tsx
@@ -135,7 +135,7 @@ export class Editor extends React.Component<IAllProps> {
     } else {
       const channel = this.props.cloudChannel;
       const apiKey = this.props.apiKey ? this.props.apiKey : 'no-api-key';
-      return `https://cdn.tiny.cloud/1/${apiKey}/tinymce/${channel}/tinymce.min.js`;
+      return ``;
     }
   }
 
